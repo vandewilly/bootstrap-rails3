9a7881db6aa28b4b37754e12a9d207ef4d2a4451
jgeiger <jgeiger@gmail.com>
2010-08-19 12:53:58 -0500

    use only a single option

--- app.rb
+++ app.rb
@@ -1,3 +1,8 @@
+# Remove normal files we don't want
+%w(README public/index.html public/favicon.ico public/robots.txt public/images/rails.png).each do |f|
+  remove_file f
+end
+
 rvmrc = <<-RVMRC
 rvm_gemset_create_on_use_flag=1
 rvm gemset use #{app_name}
@@ -5,44 +10,105 @@ RVMRC
 
 create_file ".rvmrc", rvmrc
 
-gem "factory_girl_rails", ">= 1.0.0", :group => :test
-gem "factory_girl_generator", ">= 0.0.1", :group => [:test, :development]
-gem "haml-rails", ">= 0.0.2"
-gem "rspec-rails", ">= 2.0.0.beta.12", :group => :test
+# Gems, listed in alpha order
+
+gem 'rails', '3.0.0.rc'
+gem 'mysql2'
+gem 'haml'
+gem 'haml-rails', '>= 0.0.2'
+gem 'warden'
+gem 'devise', :git => 'git://github.com/plataformatec/devise.git'
+gem 'bcrypt-ruby', :require => 'bcrypt'
+gem 'oauth2'
+gem 'will_paginate', '3.0.pre2'
+gem "jammit"
+gem 'admin_data'
+
+gem 'capybara', :group => [:test, :cucumber]
+gem 'database_cleaner', :group => [:test, :cucumber]
+gem 'cucumber-rails', :group => [:test, :cucumber]
+gem 'cucumber', :group => [:test, :cucumber]
+gem 'spork', :group => [:test, :cucumber]
+gem 'launchy', :group => [:test, :cucumber]    # So you can do Then show me the page
+gem 'webrat', :group => [:test, :cucumber]
+gem 'rspec', '>= 2.0.0.beta.19', :group => [:test, :cucumber]
+gem 'rspec-rails', '>= 2.0.0.beta.19', :group => [:development, :test, :cucumber]
 
 generators = <<-GENERATORS
-
-    config.generators do |g|
-      g.test_framework :rspec, :fixture => true, :views => false
-      g.integration_tool :rspec, :fixture => true, :views => true
-    end
+  config.generators do |g|
+    g.template_engine :haml
+    g.test_framework :rspec, :fixture => true, :views => false
+  end
 GENERATORS
 
 application generators
 
-get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js",  "public/javascripts/jquery.js"
-get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.1/jquery-ui.min.js", "public/javascripts/jquery-ui.js"
-get "http://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
+get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js",  "public/javascripts/jquery/jquery.js"
+get "http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.4/jquery-ui.min.js", "public/javascripts/jquery/jquery-ui.js"
+get "http://github.com/jgeiger/blockui/raw/master/jquery.blockUI.js", "public/javascripts/jquery/jquery.blockUI.js"
+get "http://github.com/documentcloud/underscore/raw/master/underscore.js", "public/javascripts/lib/underscore.js"
+get "http://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/lib/rails.js"
 
-gsub_file 'config/application.rb', 'config.action_view.javascript_expansions[:defaults] = %w()', 'config.action_view.javascript_expansions[:defaults] = %w(jquery.js jquery-ui.js rails.js)'
+get "http://yui.yahooapis.com/combo?3.1.1/build/cssreset/reset-min.css&3.1.1/build/cssfonts/fonts-min.css&3.1.1/build/cssgrids/grids-min.css", "public/stylesheets/reset-fonts-grids.css"
+get "http://yui.yahooapis.com/3.1.1/build/cssbase/base-min.css", "public/stylesheets/base.css"
 
-layout = <<-LAYOUT
-!!!
-%html
-  %head
-    %title #{app_name.humanize}
-    = stylesheet_link_tag :all
-    = javascript_include_tag :defaults
-    = csrf_meta_tag
-  %body
-    = yield
-LAYOUT
+jammit = <<-JAMMIT
+embed_assets: on
+javascript_compressor: closure # (yui, closure)
 
-remove_file "app/views/layouts/application.html.erb"
-create_file "app/views/layouts/application.html.haml", layout
+javascripts:
+  common:
+    - public/javascripts/jquery/jquery.js
+    - public/javascripts/jquery/jquery-ui.js
+    - public/javascripts/jquery/jquery.blockUI.js
+    - public/javascripts/lib/underscore.js
+    - public/javascripts/lib/rails.js
+
+    - public/javascripts/application.js
+  admin:
+stylesheets:
+  common:
+    - public/stylesheets/reset-fonts-grids.css
+    - public/stylesheets/base.css
+    - public/stylesheets/application.css
+  admin:
+JAMMIT
+
+create_file "config/assets.yml", jammit
+
+# Layout
+# need way to inject the jammit includes into the layout
+# <%= stylesheet_link_tag :commmon, 'screen', :cache => true %>
 
 create_file "log/.gitkeep"
 create_file "tmp/.gitkeep"
+
+remove_file ".gitignore"
+gitignore = <<-GITIGNORE
+tmp/[^.]*
+log/[^.]*
+doc/app
+doc/api
+.svn
+*~
+config/database.yml
+log/*.log
+tmp/**/*
+tmp/*
+.DS\_Store
+.DS_Store
+db/*.sqlite3
+/log/*.pid
+/coverage/*
+public/system/*
+.ackrc
+public/assets/[^.]*
+config/settings.yml
+.bundle
+vendor/bundle
+GITIGNORE
+
+create_file ".gitignore", gitignore
 
 git :init
 git :add => "."
@@ -52,9 +118,11 @@ docs = <<-DOCS
 Run the following commands to complete the setup of #{app_name.humanize}:
 
 % cd #{app_name}
-% gem install bundler --pre
-% bundle install
-% script/rails generate rspec:install
+% bundle install (or 'bundle install $BUNDLER_PATH' if RVM/Passenger)
+% rails generate rspec:install
+% rails generate cucumber:install --rspec --capybara
+% rails generate devise:install
+% rails generate devise User
 
 DOCS
 
