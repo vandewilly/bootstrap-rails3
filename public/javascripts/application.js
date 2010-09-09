5592be56c573324135285146268a8a3296c09199
jgeiger <jgeiger@gmail.com>
2010-09-09 18:47:51 -0500

    setup loading image and display it properly

---
+++ public/javascripts/application.js
@@ -0,0 +1,13 @@
+$(function() {
+
+  // display the loading graphic during ajax requests
+  $("#loading").ajaxStart(function(){
+     $(this).show();
+   }).ajaxStop(function(){
+     $(this).hide();
+   });
+
+   // make sure we accept javascript for ajax requests
+  jQuery.ajaxSetup({'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript");}});
+
+});
