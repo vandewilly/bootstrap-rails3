9cbffcaae581f043c4237a5120e3795a110b5576
jgeiger <jgeiger@gmail.com>
2010-09-27 09:28:02 -0500

    we're not using this rounded method anymore

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -57,21 +57,6 @@ module ApplicationHelper
     html.html_safe
   end
 
-  # Only need this helper once, it will provide an interface to convert a block into a partial.
-    # 1. Capture is a Rails helper which will 'capture' the output of a block into a variable
-    # 2. Merge the 'body' variable into our options hash
-    # 3. Render the partial with the given options hash. Just like calling the partial directly.
-  def block_to_partial(partial_name, options = {}, &block)
-    options.merge!(:body => capture(&block))
-    concat(render(:partial => partial_name, :locals => options))
-  end
-
-  # Create as many of these as you like, each should call a different partial
-    # 1. Render 'shared/rounded_box' partial with the given options and block content
-  def rounded_box(css_class, options = {}, &block)
-    block_to_partial('shared/rounded_box', options.merge(:css_class => css_class), &block)
-  end
-
   def page_title(title)
     content_tag(:div, title, :class => 'page-title')
   end
