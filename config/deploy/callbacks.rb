730623ca289bd128a1354e441ce8a2570ec88db6
jgeiger <jgeiger@gmail.com>
2010-09-28 10:07:53 -0500

    use scss for stylesheets

--- config/deploy/callbacks.rb
+++ config/deploy/callbacks.rb
@@ -5,13 +5,17 @@ namespace :deploy do
     run "cd #{current_path}; bundle install --deployment --without test cucumber"
   end
 
-  desc 'Bundle and minify the JS and CSS files'
+  desc 'Compile, bundle and minify the JS and CSS files'
   task :precache_assets, :roles => :app do
     root_path = File.expand_path(File.dirname(__FILE__) + '/../..')
+    sass_path = "#{root_path}/public/stylesheets/sass/"
+    css_path = "#{root_path}/public/stylesheets/"
+    run_locally "bundle exec #{root_path}/vendor/bundle/ruby/1.9.1/bin/sass --update #{sass_path}:#{css_path}"
     assets_path = "#{root_path}/public/assets"
     run_locally "bundle exec #{root_path}/vendor/bundle/ruby/1.9.1/bin/jammit"
     top.upload assets_path, "#{current_path}/public", :via => :scp, :recursive => true
   end
+
 
 end
 
