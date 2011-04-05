0d9beef7986a83d58fa17504e8e4399d9c87b805
jgeiger <jgeiger@gmail.com>
2011-04-05 14:19:08 -0500

    update layouts

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -10,4 +10,8 @@ $(function() {
    // make sure we accept javascript for ajax requests
   jQuery.ajaxSetup({'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript");}});
 
+  if ($('#flash-messages').children().length > 0) {
+    $('#flash-messages').show().delay(1000).fadeOut(1500);
+  }
+
 });
