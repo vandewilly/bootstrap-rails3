561b2627fa19be5f5b3eb11d79a425e176097724
jgeiger <jgeiger@gmail.com>
2011-09-23 23:20:41 -0500

    update for rails 3.1 precompiling

--- config/deploy/callbacks.rb
+++ config/deploy/callbacks.rb
@@ -5,15 +5,10 @@ namespace :deploy do
     run "cd #{current_path}; bundle install --gemfile Gemfile --deployment --without cucumber development test"
   end
 
-  desc 'Compile, bundle and minify the JS and CSS files'
-  task :precache_assets, :roles => :app do
-    root_path = File.expand_path(File.dirname(__FILE__) + '/../..')
-    sass_path = "#{root_path}/public/stylesheets/sass/"
-    css_path = "#{root_path}/public/stylesheets/"
-    run_locally "bundle exec #{root_path}/vendor/bundle/ruby/1.9.1/bin/sass --update #{sass_path}:#{css_path}"
-    assets_path = "#{root_path}/public/assets"
-    run_locally "bundle exec #{root_path}/vendor/bundle/ruby/1.9.1/bin/jammit"
-    top.upload assets_path, "#{current_path}/public", :via => :scp, :recursive => true
+  desc "precompile the assets"
+  task :precompile_assets, :roles => :web, :except => { :no_release => true } do
+    run "cd #{current_path}; rm -rf public/assets/*"
+    run "cd #{current_path}; RAILS_ENV=production bundle exec rake assets:precompile"
   end
 
 end
