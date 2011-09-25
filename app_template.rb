25871792b78c5cd3072f21001ce85911cc7e7f04
jgeiger <jgeiger@gmail.com>
2011-09-24 19:41:18 -0500

    include bundler

--- app_template.rb
+++ app_template.rb
@@ -18,6 +18,8 @@ gem 'oauth2'
 gem 'kaminari'
 gem 'simple_form'
 gem 'uuidtools'
+gem 'bundler'
+gem 'bundler-ext'
 
 gem "capybara", :group => [:test, :cucumber]
 gem 'database_cleaner', :group => [:test, :cucumber]
