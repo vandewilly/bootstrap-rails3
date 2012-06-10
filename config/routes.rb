e64d67913774c8947750571bf12da20403adfb41
jgeiger <jgeiger@gmail.com>
2012-06-10 15:07:56 -0500

    default auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 APP_NAME::Application.routes.draw do
 
+  match "/auth/:provider/callback", to: "sessions#create"
+  match "/auth/failure", to: "sessions#failure"
+  match "/sign_out", to: "sessions#destroy", :as => "sign_out"
+  match "/sign_in", to: "sessions#new", :as => "sign_in"
+  match "/dashboard", to: "users#dashboard", :as => "dashboard"
+
+  resources :sessions
+  resources :users
+
   root :to => 'pages#home'
 
 end
